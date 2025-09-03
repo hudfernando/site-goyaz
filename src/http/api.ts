c8e9cc0d5bf541b52bd3109ef7e86a5bf189687a
Hudson Fernando <hudfernando@gmail.com>
2025-09-03 13:05:34 -0300

    sexto

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,7 +1,7 @@
 import ky from 'ky'
 //baseURL: 'http://177.39.16.76:8405',
 export const api = ky.create({
-  prefixUrl: 'http://45.70.40.98:3718'
+  prefixUrl: 'http://177.39.16.76:3718'
   //prefixUrl: 'https://localhost:7286',
   //prefixUrl: 'http://177.39.16.76:8407',
 })
